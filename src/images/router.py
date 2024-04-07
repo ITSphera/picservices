24abbc4c8568fee870c2376c2ac0616640f9ed3d
vbamnup <vbamnup@gmail.com>
2024-04-07 15:46:56 +0300

    data in body

--- src/images/router.py
+++ src/images/router.py
@@ -1,10 +1,17 @@
 import asyncio
 from pathlib import Path
 from celery.result import AsyncResult
+from typing import Any, Union
+from PIL import Image
+from io import BytesIO
 
-from fastapi import File, UploadFile
+from fastapi import File, UploadFile, Depends
 
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+
+from .models import UploadData
 from src.tasks import process_image
 from src.config import SERVICES, celery, BASE_URL
 
@@ -14,31 +21,45 @@ router = APIRouter()
 
 @router.post("/upload")
 async def upload_image(
-    service: str, target_type: str, file: UploadFile = File(...)
-) -> dict:
+    upload_data: UploadData = Depends(), file: UploadFile = File(...)
+) -> Union[dict, Any]:
     """
     Upload image to the server
-
     Args:
+        upload_data (UploadData): The service and target type for image processing.
         file (UploadFile): The image file to be uploaded.
-        service (str): The service to use for image processing.
-        target_type (str): The target type for image processing.
-
     Returns:
         dict: A dictionary containing the task ID and message.
     """
+    service = upload_data.service
+    target_type = upload_data.target_type
 
     if service not in SERVICES:
-        return {"error": f"Service '{service}' not found"}
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"error": f"Service '{service}' not found"},
+        )
     if target_type not in SERVICES[service]:
-        return {
-            "error": f"Target type '{target_type}' not found for service '{service}'"
-        }
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={
+                "error": f"Target type '{target_type}' not found for service '{service}'"
+            },
+        )
 
     file: bytes = await file.read()
     target_dir: str = SERVICES[service][target_type]["dir"]
     width: int = SERVICES[service][target_type]["width"]
-    task: asyncio.Task = process_image.delay(file, target_dir, width)
+
+    try:
+        # Проверить что файл является изображением
+        with Image.open(BytesIO(file)) as _:
+            task: asyncio.Task = process_image.delay(file, target_dir, width)
+    except Exception:
+        return JSONResponse(
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            content={"error": "Error processing image: File is not an image"},
+        )
 
     return {"task_id": task.id, "message": "Image processing started"}
 
