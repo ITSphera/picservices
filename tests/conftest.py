a065a5465d511622d928df74691c8c8991cc3c40
vbamnup <vbamnup@gmail.com>
2024-04-07 14:37:33 +0300

    moved the fixtures to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import os
+
+import pytest
 import pytest_asyncio
+from PIL import Image
 from httpx import AsyncClient
 
 from main import app
@@ -12,3 +16,42 @@ async def async_client():
         app=app, base_url="http://test"
     ) as async_test_client:
         yield async_test_client
+
+
+@pytest.fixture(scope="session")
+def image():
+    """
+    Create test image
+    :return:
+    """
+    image = Image.new("RGB", (100, 50))
+    image.save("test.png")
+    image = bytes(open("test.png", "rb").read())
+    yield image
+    os.remove("test.png")
+
+
+@pytest.fixture(scope="session")
+def not_image():
+    """
+    Create not image
+    :return:
+    """
+    with open("test.txt", "w+") as my_file:
+        my_file.write("Привет, файл!")
+    not_image = bytes(open("test.txt", "rb").read())
+    yield not_image
+    os.remove("test.txt")
+
+
+@pytest.fixture(scope="session")
+def image_height_more_than_1080():
+    """
+    Create test image with height more than 1080
+    :return:
+    """
+    image = Image.new("RGB", (100, 2000))
+    image.save("test2000.png")
+    image = bytes(open("test2000.png", "rb").read())
+    yield image
+    os.remove("test2000.png")
--- tests/images/test_image_handler.py
+++ tests/images/test_image_handler.py
@@ -1,49 +1,9 @@
-import os
 import shutil
 
 import pytest
 from PIL import Image, UnidentifiedImageError
 
 from src.images.image_handler import image_handler
-
-
-@pytest.fixture(scope="session")
-def image():
-    """
-    Create test image
-    :return:
-    """
-    image = Image.new("RGB", (100, 50))
-    image.save("test.png")
-    image = bytes(open("test.png", "rb").read())
-    yield image
-    os.remove("test.png")
-
-
-@pytest.fixture(scope="session")
-def not_image():
-    """
-    Create not image
-    :return:
-    """
-    with open("test.txt", "w+") as my_file:
-        my_file.write("Привет, файл!")
-    not_image = bytes(open("test.txt", "rb").read())
-    yield not_image
-    os.remove("test.txt")
-
-
-@pytest.fixture(scope="session")
-def image_height_more_than_1080():
-    """
-    Create test image with height more than 1080
-    :return:
-    """
-    image = Image.new("RGB", (100, 2000))
-    image.save("test2000.png")
-    image = bytes(open("test2000.png", "rb").read())
-    yield image
-    os.remove("test2000.png")
 
 
 def test_image_handler(image):
