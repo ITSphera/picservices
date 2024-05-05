0f377bf488646b93dd6aa8a65773bcac89ad8493
vbamnup <vbamnup@gmail.com>
2024-05-05 22:38:53 +0300

    fix docker

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,4 +7,4 @@ echo "Starting Celery"
 celery -A src.tasks worker --loglevel=info &
 
 echo "Starting uvicorn"
-uvicorn src.main:app --host 0.0.0.0 --port 8000
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --log-config log/logconfig.ini
