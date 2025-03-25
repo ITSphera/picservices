960790110f4da4b53cf60105c6e88108a40d8f99
Sergio N. <me@sergio.su>
2025-03-26 01:03:34 +0300

    Ver. 0.1

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-set -x
-echo "Starting Image Service ..."
-
-echo "Starting Celery"
-celery -A src.tasks worker --loglevel=info &
-
-echo "Starting uvicorn"
-uvicorn src.main:app --host 0.0.0.0 --port 8000 --log-config log/logconfig.ini
+# set -x
+# echo "Starting uvicorn"
+# uvicorn src.main:app --host 0.0.0.0 --port 8000 --log-config log/logconfig.ini
+which uvicorn
